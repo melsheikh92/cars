ea0cd645d2aa9ba95b7b26e2b45f7d19906c4638
Mahmoud <m.sheikh@egypt.itgsolutions.com>
2018-11-15 17:56:01 +0200

    - added gesture

--- Cars/Cars/ViewController/ViewController.swift
+++ Cars/Cars/ViewController/ViewController.swift
@@ -17,8 +17,11 @@ class ViewController: UIViewController {
         self.stackviewNav.separatorColor = UIColor(named: "grayColor")
         self.stackviewNav.separatorLength = 25.0
         self.stackviewNav.separatorThickness = 1.0
-   
+         let v = UIView()
+     let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+     v.addGestureRecognizer(tap)
     }
+    @objc func handleTap(){}
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
        
